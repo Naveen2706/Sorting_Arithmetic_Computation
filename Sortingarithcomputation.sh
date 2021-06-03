be2203326e36fc607aec9a7cc0b8a779bd00f8f3
Naveen2706 <naveen271996@gmail.com>
2021-06-04 00:09:14 +0530

    computes stored in dictionary

--- Sortingarithcomputation.sh
+++ Sortingarithcomputation.sh
@@ -1,17 +1,28 @@
 This problem computes different arithmetic expressions and Sorts the results
+declare -A arith
 
 read a
 read b
 read c
+i=0
 
 operation1=$(($a+$(($b*$c))))
 echo result of a+b*c :$operation1
+arith[$i]=$operation1
+((i++))
 
 operation2=$(($c+$(($a*$b))))
 echo result of a*b+c :$operation2
+arith[$i]=$operation2
+((i++))
 
 operation3=$(($c+$(($a/$b))))
 echo result of c+a/b :$operation3
+arith[$i]=$operation3
+((i++))
 
 operation4=$(($c+$(($a%$b))))
 echo result of a%b+c :$operation4
+arith[$i]=$operation4
+
+
